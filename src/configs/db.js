c017c170bf179834784f47cf14482ee2314c2c8f
megha9354 <rawatmegha9354@gmail.com>
2023-08-07 22:56:38 +0530

    change

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-
+const URL =
+  process.env.MONGO_URI ||
+  "mongodb+srv://rawatmegha9354:PYduvzClwLiJoJsU@cluster0.jlkodts.mongodb.net/eval4";
 module.exports = () => {
-  return mongoose.connect(process.env.MONGO_URI);
+  return mongoose.connect(URL);
 };
--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const app = require("./index");
 const connect = require("./configs/db");
 require("dotenv").config();
 
-app.listen(process.env.PORT, async (req, res) => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, async (req, res) => {
   try {
     await connect();
-    console.log("LISTENING ON PORT " + process.env.PORT);
+    console.log("LISTENING ON PORT " + PORT);
   } catch (err) {
     return res.status(501).send(err.message);
   }
